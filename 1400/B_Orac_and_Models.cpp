d096fddadd41ad539588339326da0fde2c03b6bb
HardikNegi9 <hardiknegi764isc@gmail.com>
2025-02-02 21:40:47 +0530

    1400-21

--- 1400/B_Orac_and_Models.cpp
+++ 1400/B_Orac_and_Models.cpp
@@ -2,16 +2,15 @@
 #include<vector>
 using namespace std;
 
+long long solve(int i, vector<long long> &a, vector<long long> &dp, int n){
+    if(i > n) return 0;
+    if(dp[i] != -1) return dp[i];
+    long long res = 1;
+    for(int j = i + i; j <= n; j += i){
+        if(a[i] < a[j]) res = max(solve(j, a, dp, n) + 1, res);
+    }
 
-long long solve(int n, int i, vector<int> a, int prev, vector<vector<long long>> &dp){
-    if(i == n) return 0;
-    if(dp[i][prev + 1] != -1) return dp[i][prev + 1];
-    
-    int not_take = solve(n, i + 1, a, prev, dp);
-    int take = 0;
-    if(prev == -1 || a[i] % a[prev] == 0) take = solve(n, i + 1, a, i, dp) + 1;
-
-    return dp[i][prev + 1] = max(take, not_take);
+    return dp[i] = res;
 }
 
 int main(){
@@ -20,16 +19,15 @@ int main(){
     while(t--){
         int n;
         cin >> n;
-        vector<long long> a(n);
-        for(int i = 0; i < n; i++){
+        vector<long long> a(n + 1);
+        for(int i = 1; i <= n; i++){
             cin >> a[i];
         }
 
-        vector<vector<long long>> dp(n, vector<long long>(n + 1, -1));
+        vector<long long> dp(n + 1, -1);
+        long long res = 1;
 
-        int res = solve(n, 0, a, -1, dp);
-
-        cout << res << endl;
-
+        for(int i = 1; i < n; i++) res = max(res, solve(i, a, dp, n));
+        cout << res << endl;    
     }
 }
