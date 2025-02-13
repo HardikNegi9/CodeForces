f552a7cd1266f39724e071794689ad83fbbb8c26
HardikNegi9 <hardiknegi764isc@gmail.com>
2025-02-13 13:15:34 +0530

    1400-31 - sol2

--- 1400/F_Greetings.cpp
+++ 1400/F_Greetings.cpp
@@ -1,12 +1,48 @@
 #include<bits/stdc++.h>
 
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
-using namespace __gnu_pbds;
+// using namespace __gnu_pbds;
 
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
+
+long long merge_sort(vector<int> & nums, int n, int low, int high){
+    if(low >= high){
+        return 0;
+    }
+
+    int mid = low + (high - low) / 2;
+    long long res = merge_sort(nums, n, low, mid) + merge_sort(nums, n, mid + 1, high);
+
+    vector<int> temp(high - low + 1);
+    int i = low, j = mid + 1, k = 0;
+    while(i <= mid && j <= high){
+        if(nums[i] <= nums[j]){
+            temp[k++] = nums[i++];
+        }else{
+            res += mid - i + 1;
+            temp[k++] = nums[j++];
+        }
+    }
+
+    while(i <= mid){
+        temp[k++] = nums[i++];
+    }
+
+    while(j <= high){
+        temp[k++] = nums[j++];
+    }
+
+    for(int i = low; i <= high; i++){
+        nums[i] = temp[i - low];
+    }
+
+    return res;
+}
+
 
 int main() {
 
@@ -16,23 +52,30 @@ int main() {
         int n;
         cin >> n;
         vector<pair<int, int> > v(n);
-    	pbds A; // declaration
+    	// pbds A; // declaration
 
         for(int i = 0; i < n; i++){
             cin >> v[i].first >> v[i].second;
-            A.insert(v[i].second);
+            // A.insert(v[i].second);
         }
         
-        long long res = 0;
+        // long long res = 0;
 
-        sort(v.begin(), v.end());
+        sort(v.begin(), v.end()); // common in both
 
+        // for(int i = 0; i < n; i++){
+        //     res += A.order_of_key(v[i].second);
+        //     A.erase(v[i].second);
+        // }
+
+        // cout << res << endl;
+
+        vector<int> nums(n);
         for(int i = 0; i < n; i++){
-            res += A.order_of_key(v[i].second);
-            A.erase(v[i].second);
+            nums[i] = v[i].second;
         }
 
-        cout << res << endl;
+        cout << merge_sort(nums, n, 0, n - 1) << endl;   
         
     }
 
