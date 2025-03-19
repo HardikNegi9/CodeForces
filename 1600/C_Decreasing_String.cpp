364f73a87a88c3999a714bee90607719ea831cea
HardikNegi9 <hardiknegi764isc@gmail.com>
2025-03-19 20:45:50 +0530

    1600-3

--- 1600/C_Decreasing_String.cpp
+++ 1600/C_Decreasing_String.cpp
@@ -1,49 +1,61 @@
-#include<iostream>
-#include<stack>
-#include<vector>
-#include<set>
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <set>
 using namespace std;
 
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        string s;
-        cin >> s;
-        int pos;
-        cin >> pos;
+void solve() {
+    string s;
+    cin >> s;
 
-        stack<char> stk;
-        vector<int> v;
-        set<char> st;
+    long long pos;
+    cin >> pos;
 
-        for(char c : s){
-            while(stk.size() && stk.top() > c){
-                v.push_back(stk.top());
-                stk.pop();
-            }
-            stk.push(c);
-        }
+    stack<long long> stk;
+    vector<long long> v;
 
-        while(stk.size()){
+    // Generate the lexicographically minimal sequence
+    for (long long i = 0; i < s.size(); i++) {
+        while (!stk.empty() && s[stk.top()] > s[i]) {
             v.push_back(stk.top());
             stk.pop();
         }
+        stk.push(i);
+    }
 
-        int n = s.size();
-        int i = 0;
+    while (!stk.empty()) {
+        v.push_back(stk.top());
+        stk.pop();
+    }
 
-        while(n < pos){
-            pos -= n;
-            n--;
-            st.insert(v[i]);
-            i++;
-        }
+    long long n = s.size();
+    set<long long> st;
+    long long p = 0;
 
-        for(char c : s){
-            if(st.count(c)) continue;
-            pos--;
-            if(pos == 0) cout << c;
+    // Determine the string and position
+    while (pos > n) {
+        pos -= n;
+        n--;
+        st.insert(v[p++]);
+    }
+
+    // Find the character at position pos
+    for (long long i = 0; i < s.size(); i++) {
+        if (st.count(i)) continue;
+        pos--;
+        if (pos == 0) {
+            cout << s[i];
+            return;
         }
     }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
 }
