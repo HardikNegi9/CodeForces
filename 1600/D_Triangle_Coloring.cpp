a74fc2a68ff58d635c99a4eec23a3e7bde2a38f0
HardikNegi9 <hardiknegi764isc@gmail.com>
2025-03-26 23:06:19 +0530

    1600-8 Fix

--- 1600/D_Triangle_Coloring.cpp
+++ 1600/D_Triangle_Coloring.cpp
@@ -1,37 +1,66 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-long long nCr(int n, int r){
-    if(r > n - r) r = n - r;
+const int mod = 998244353;
+
+long long modPower(long long base, long long exponent, long long mod) {
+    long long result = 1;
+    while (exponent > 0) {
+        if (exponent & 1) {
+            result = (result * base) % mod;
+        }
+        base = (base * base) % mod;
+        exponent >>= 1;
+    }
+    return result;
+}
+
+long long modInverse(long long x, long long mod) {
+    return modPower(x, mod - 2, mod);
+}
+
+long long nCr(int n, int r, long long mod) {
+    if (r > n) return 0;
     long long res = 1;
-    for(int i = 0; i < r; i++){
-        res = res * (n - i) / (i + 1);
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i) % mod;
+        res = res * modInverse(i + 1, mod) % mod;
     }
     return res;
 }
 
-
-int main(){
+int main() {
     int n;
     cin >> n;
-    long long res = 1;
-    const int mod = 998244353;
-    for(int i = 0; i < n / 3; i++){
-        int a, b, c;
-        cin >> a >> b >> c;
-        vector<int> v = {a, b, c};
-        sort(v.begin(), v.end());
-        
-        if(v[0] == v[2]){
-            res = (res * 3) % mod;
-        } else if(v[0] == v[1] || v[1] == v[2]){
-            res = (res * 2) % mod;
+
+    if (n % 3 != 0) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    vector<int> v(n);
+    for (auto &i : v) {
+        cin >> i;
+    }
+
+    long long res = nCr(n / 3, n / 6, mod);
+    vector<int> temp;
+
+    for (int i = 0; i < n; i++) {
+        temp.push_back(v[i]);
+        if ((i + 1) % 3 == 0) {
+            sort(temp.begin(), temp.end());
+            if (temp.back() == temp.front()) {
+                res = (res * 3) % mod;
+            } else if (temp[0] == temp[1] && temp[2] > temp[0]) {
+                res = (res * 2) % mod;
+            }
+            temp.clear();
         }
     }
 
-    res = nCr(n / 3, n / 6) * res % mod;
-
-    cout << res / 2 << endl;
+    cout << res << endl;
+    return 0;
 }
